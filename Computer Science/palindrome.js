ea2752cdd63ec0f42230c05d9ed4594ca80643aa
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-21 19:03:57 +0700

    Create palindrome.js

---
+++ Computer Science/palindrome.js
@@ -0,0 +1,17 @@
+// create palindrome for
+
+const palindrome = (str) => {
+  if (typeof str !== "string" || str === "") {
+    console.log("Data tidak valid");
+  } else {
+    const toLower = str.toLowerCase();
+    const textPalindrome = toLower.split("").reverse().join("");
+    if (toLower === textPalindrome) {
+      console.log(`${toLower} adalah palindrome`);
+    } else {
+      console.log(`${toLower} bukan palindrome`);
+    }
+  }
+};
+
+palindrome("malam");
