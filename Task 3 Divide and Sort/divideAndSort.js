74476b912697bbf2ec373d0d3dcbe0c3d2b8a379
Hasbi Alwi Kusmana <hasbialwi70@gmail.com>
2022-04-25 10:57:26 +0700

    Update divideAndSort.js

--- Task 3 Divide and Sort/divideAndSort.js
+++ Task 3 Divide and Sort/divideAndSort.js
@@ -3,8 +3,15 @@ const divideAndSort = (num) => {
     console.log("input data dengan tipe data Number");
   } else {
     const numStr = num.toString().split("0");
-    const numMap = numStr.map((item) => item.split("").sort().join(""));
-    const numHasil = parseInt(numMap.join(""));
+    const numMap = numStr
+      .map((item) =>
+        item
+          .split("")
+          .sort((a, b) => a - b)
+          .join("")
+      )
+      .join("");
+    const numHasil = parseInt(numMap);
     console.log(numHasil);
     // console.log(numStr);
   }
